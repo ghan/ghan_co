efef98289b604b763001c44d8af40dfc654d4ab0
Ghan Patel <ghanpatel@gmail.com>
2020-08-27 20:28:45 -0700

    build init site

---
+++ build/precache-manifest.5e103f8b2ff58a5f87dc4e21eb0e04fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c29ac5c5fffa596c22b3c5d9992666a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14c155d145b92bc827c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d71c3786a6bf6371da8a",
+    "url": "/static/js/2.31a87ef4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31a87ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c155d145b92bc827c5",
+    "url": "/static/js/main.ee1b3067.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
