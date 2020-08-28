609bc1afb8e15644fd78e5f37c1f64b28da7188d
Ghan Patel <ghanpatel@gmail.com>
2020-08-28 12:32:42 -0700

    fixing routes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import routes from './data/routes';
 import Home from './pages/Home';
 import Zip from './pages/Zip';
 import FourOFour from './pages/FourOFour';
+import ExternalRoute from './components/ExternalRoute';
 
 export default function App() {
+  const list = routes.map((r) => {
+    if (r.hasOwnProperty('url')) {
+      return (
+        <Route key={r.tag} exact path={`/${r.tag}`}>
+          <ExternalRoute route={r} />
+        </Route>
+      );
+    } else {
+      return null;
+    }
+  });
+
   return (
     <Router>
       <Switch>
+        { list }
         <Route exact path="/zip">
           <Zip />
         </Route>
