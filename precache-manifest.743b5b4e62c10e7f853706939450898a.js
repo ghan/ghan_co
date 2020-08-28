4331dedb644c0a3a65e1fc17b12cb4835aa58e4a
Ghan Patel <ghanpatel@gmail.com>
2020-08-27 20:37:03 -0700

    Updates

---
+++ precache-manifest.743b5b4e62c10e7f853706939450898a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f367b17e9fbaf6a07e454ce0261dafd9",
+    "url": "/ghan_co/index.html"
+  },
+  {
+    "revision": "08d06f8532a2ce328442",
+    "url": "/ghan_co/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f60825298460900ef270",
+    "url": "/ghan_co/static/js/2.21d9e552.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ghan_co/static/js/2.21d9e552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d06f8532a2ce328442",
+    "url": "/ghan_co/static/js/main.2312530d.chunk.js"
+  },
+  {
+    "revision": "dd384d9d329a6fe3841f",
+    "url": "/ghan_co/static/js/runtime-main.7667e061.js"
+  }
+]);
